61bf3b9341f20ca65787fc26f7b78928c377ca71
justintjoa <justintoja@gmail.com>
2021-03-04 21:43:01 -0800

    got skeleton down

--- src/main/scala/MemoryCell/MemoryCell.scala
+++ src/main/scala/MemoryCell/MemoryCell.scala
@@ -1,5 +1,17 @@
 package MemoryCell
 
-class MemoryCell {
+case class MemoryCell(var array_handle:Int,
+var array_code:Int, var index:Int,
+var value:Int, var rank:Int,
+var low:Int,
+var high:Int) {
 
+
+  var arrDef:Boolean = false;
+
+
+  var eltDef:Boolean = false;
+
+
+  //notify, zombie, select, and mark will be implemented in the lowest level API
 }
--- src/main/scala/System/System.scala
+++ src/main/scala/System/System.scala
@@ -1,5 +1,19 @@
 package System
 
+import MemoryCell.MemoryCell
+
 class System {
 
+  private var sysArray = new Array[MemoryCell](100)
+
+  def tFold(): Unit = {
+
+  }
+
+  def tMap() {
+  }
+
+  def tScanL: Unit = {
+
+  }
 }
