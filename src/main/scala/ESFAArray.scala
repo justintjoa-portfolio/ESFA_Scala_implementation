cd50c8cb57e642858027b655187c8b785b572d95
justintjoa <justintoja@gmail.com>
2021-03-15 21:17:47 -0700

    clarified logic for updateS

--- src/main/scala/ESFAArray.scala
+++ src/main/scala/ESFAArray.scala
@@ -4,45 +4,17 @@ import Circuit.ESFMachine
 case class ESFAArray(esfMachine: ESFMachine) {
 
 
-  def empty(): Option[ESFAArray] = {
+  def update(identifier:Int, index:Int, value:Int): Unit = {
+
+  }
+
+  def lookUp(identifier: Int, index:Int): Option[Int] = {
     return None
   }
-
-  def update(handle:Int, index:Int, value:Int, isPointerToHeap:Boolean): Unit = {
-    print("hello!")
-  }
-
-  def lookUp(handle: Int, index:Int): Option[Int] = {
-    return None
-  }
-
-  def minDef: Option[Int] = {
-    return None
-  }
-
-  def maxDef: Option[Int] = {
-    return None
-  }
-
-  def nextDef: Option[Int] = {
-    return None
-  }
-
-  def prevDef: Option[Int] = {
-    return None
-  }
-
-
-
-
- def delete(): Unit = {
-
- }
-
-  def killZombies(): Unit = {
-
-  }
-
-
 
 }
+
+
+
+
+
